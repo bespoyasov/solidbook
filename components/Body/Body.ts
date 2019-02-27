0f64c5016efcd5b5910957adbd332ca6fdbaf923
Artem Samofalov <dex157@gmail.com>
2019-02-27 22:21:09 +0300

    remove font-face styles from body tag

--- components/Body/Body.ts
+++ components/Body/Body.ts
@@ -1,23 +1,3 @@
 import styled from 'styled-components'
 
-export default styled.body`
-  @font-face {
-    font-family: SF UI Text;
-    src: url(./Fonts/SFUIText-Regular.eot);
-    src: local('SF UI Text Regular'), local('SFUIText-Regular'),
-      url(./Fonts/SFUIText-Regular.eot?#iefix) format('embedded-opentype'),
-      url(./Fonts/SFUIText-Regular.woff) format('woff'), url(./Fonts/SFUIText-Regular.ttf) format('truetype');
-    font-weight: 400;
-    font-style: normal;
-  }
-
-  @font-face {
-    font-family: SF UI Text;
-    src: url(./Fonts/SFUIText-Bold.eot);
-    src: local('SF UI Text Bold'), local('SFUIText-Bold'),
-      url(./Fonts/SFUIText-Bold.eot?#iefix) format('embedded-opentype'), url(./Fonts/SFUIText-Bold.woff) format('woff'),
-      url(./Fonts/SFUIText-Bold.ttf) format('truetype');
-    font-weight: 700;
-    font-style: normal;
-  }
-`
+export default styled.body``
