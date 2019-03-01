9936194d0fb9fe8852755b8e55a7b03cc435cccd
Александр <forjob159@gmail.com>
2019-03-01 16:42:20 +0200

    add main content component

---
+++ components/MainContent/MainContent.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export default styled.main`
+  margin: auto;
+  max-width: 800px;
+  padding: 4rem 2.2rem;
+
+  h1,
+  h2,
+  h3 {
+    font-weight: 400;
+  }
+
+  h1 {
+    font-size: 2.6rem;
+  }
+
+  p {
+    margin-top: 1em;
+    margin-bottom: 1em;
+  }
+`
