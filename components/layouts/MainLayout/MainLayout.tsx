0becba959ed0a0006649cb8cbcaf3121c7aa89b4
Александр <forjob159@gmail.com>
2019-03-01 18:32:13 +0200

    add basic grid styles

--- components/layouts/MainLayout/MainLayout.tsx
+++ components/layouts/MainLayout/MainLayout.tsx
@@ -1,8 +1,25 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import { PureComponent } from 'react'
 import Head from 'next/head'
 import Header from '../../Header'
 import Center from '../Center'
+
+const Grid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  padding: 2.6rem 0;
+`
+
+const Nav = styled.nav`
+  width: 30%;
+  max-width: 280px;
+`
+
+const MainContent = styled.main`
+  flex: 1;
+`
 
 type Props = {
   title?: string
@@ -21,7 +38,12 @@ class MainLayout extends PureComponent<Props> {
 
         <Header />
 
-        <Center>{children}</Center>
+        <Center>
+          <Grid>
+            <Nav>Здесь будет содержание</Nav>
+            <MainContent>{children}</MainContent>
+          </Grid>
+        </Center>
 
         <footer>
           <Center>I`m here to stay</Center>
