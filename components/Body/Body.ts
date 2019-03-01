fce26b5beb7728b8c5976104ed69406ca89097e1
Александр <forjob159@gmail.com>
2019-03-01 16:41:59 +0200

    update default font settings

--- components/Body/Body.ts
+++ components/Body/Body.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 
 export default styled.body`
   font-family: Palatino, 'Palatino Linotype', 'Palatino LT STD', Georgia, serif;
-  font-size: 1.3rem;
-  line-height: 1.3em;
+  font-size: 1.1rem;
+  line-height: 1.4em;
 `
