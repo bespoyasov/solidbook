b994039fc5981f2c5f49825e6273ef81e7409df2
Александр <forjob159@gmail.com>
2019-03-01 16:26:37 +0200

    add basic font styles

--- components/Body/Body.ts
+++ components/Body/Body.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
-export default styled.body``
+export default styled.body`
+  font-family: Palatino, 'Palatino Linotype', 'Palatino LT STD', Georgia, serif;
+  font-size: 1.3rem;
+  line-height: 1.3em;
+`
