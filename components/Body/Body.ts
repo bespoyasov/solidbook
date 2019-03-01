55ad7cabe632a81f619eff1f02617d722f42ef68
Александр <forjob159@gmail.com>
2019-03-01 17:48:18 +0200

    add default typography styles

--- components/Body/Body.ts
+++ components/Body/Body.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components'
 
-export default styled.body``
+export default styled.body`
+  background: white;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
+    'Droid Sans', 'Helvetica Neue', sans-serif;
+  font-size: 1rem;
+  line-height: 1.65;
+  font-feature-settings: 'kern';
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+
+  p {
+    margin: 1em 0;
+  }
+`
