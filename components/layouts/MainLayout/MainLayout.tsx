5f731173fc2f77dddfbcc33745709c0e4320f740
Александр <forjob159@gmail.com>
2019-03-01 16:26:29 +0200

    delete obsolete wrappers

--- components/layouts/MainLayout/MainLayout.tsx
+++ components/layouts/MainLayout/MainLayout.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { PureComponent } from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
 
 type Props = {
@@ -17,25 +16,8 @@ class MainLayout extends PureComponent<Props> {
         <Head>
           <title>{title}</title>
         </Head>
-        <header>
-          <nav>
-            <Link href="/">
-              <a>Home</a>
-            </Link>{' '}
-            |
-            <Link href="/about">
-              <a>About</a>
-            </Link>{' '}
-            |
-            <Link href="/contact">
-              <a>Contact</a>
-            </Link>
-          </nav>
-        </header>
 
         {children}
-
-        <footer>I`m here to stay</footer>
       </div>
     )
   }
