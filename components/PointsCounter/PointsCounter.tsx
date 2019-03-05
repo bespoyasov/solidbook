224373f3df4511955918d57c295536de08a23734
Александр <forjob159@gmail.com>
2019-03-05 15:26:51 +0200

    add PointsCounter component

---
+++ components/PointsCounter/PointsCounter.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import { PureComponent } from 'react'
+import Container from './style'
+
+class Navigation extends PureComponent {
+  render() {
+    return <Container>0/1000</Container>
+  }
+}
+
+export default Navigation
---
+++ components/PointsCounter/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export default styled.article`
+  padding: 1rem;
+  border-radius: 7px;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.12);
+`
