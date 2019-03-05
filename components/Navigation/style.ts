c04e3134dbf80ecc1dbefc91fc700dbe75dc8080
Александр <forjob159@gmail.com>
2019-03-05 16:47:47 +0200

    add nav styles

--- components/Navigation/style.ts
+++ components/Navigation/style.ts
@@ -4,14 +4,44 @@ export const Nav = styled.nav`
   width: 30%;
   max-width: 280px;
   padding-right: 35px;
+  position: relative;
+
+  &::before {
+    content: ' ';
+    display: block;
+    position: absolute;
+    width: 1px;
+    top: 0;
+    bottom: 0;
+    left: 7px;
+    background: ${props => props.theme.lightestGrey};
+    z-index: -1;
+  }
 `
 
 export const NavSection = styled.div`
   margin: 2.5rem 0 0 0;
+  padding: 0 0 0 25px;
+  font-size: 0.9rem;
+
+  a,
+  a:visited {
+    color: black;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: ${props => props.theme.red};
+  }
 
   ul {
-    margin: -1rem 0 0 0;
-    list-style: none;
+    margin: 0;
     padding: 0;
+    list-style: none;
+  }
+
+  li {
+    line-height: 1.2em;
+    margin: 0.8em 0;
   }
 `
--- components/PointsCounter/style.ts
+++ components/PointsCounter/style.ts
@@ -4,4 +4,5 @@ export default styled.article`
   padding: 1rem;
   border-radius: 7px;
   box-shadow: 0 2px 10px ${props => props.theme.lightGrey};
+  background: white;
 `
