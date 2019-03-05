8aac89a8ad667bcd7aa78fd1b7292bf54a26d784
Александр <forjob159@gmail.com>
2019-03-05 17:10:27 +0200

    update nav width

--- components/Navigation/style.ts
+++ components/Navigation/style.ts
@@ -2,8 +2,8 @@ import styled from 'styled-components'
 
 export const Nav = styled.nav`
   width: 30%;
-  max-width: 280px;
-  padding-right: 35px;
+  max-width: 300px;
+  padding-right: 40px;
   position: relative;
 
   &::before {
