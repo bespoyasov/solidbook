078c35f1aad55f68711f2a00d7a6f4606bd94b32
Александр <forjob159@gmail.com>
2019-03-05 16:40:17 +0200

    add basic nav example

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,13 +1,50 @@
 import * as React from 'react'
 import { PureComponent } from 'react'
+import Link from 'next/link'
 import PointsCounter from '../PointsCounter'
-import Nav from './style'
+import { Nav, NavSection } from './style'
 
 class Navigation extends PureComponent {
   render() {
     return (
       <Nav>
         <PointsCounter />
+
+        <NavSection>
+          <h3>Содержание</h3>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Введение</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Принцип единственной ответственности</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Принцип открытости и закрытости</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Принцип подстановки Барбары Лисков</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Принцип разделения интерфейса</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>Принцип инверсии зависимостей</a>
+              </Link>
+            </li>
+          </ul>
+        </NavSection>
       </Nav>
     )
   }
--- components/Navigation/style.ts
+++ components/Navigation/style.ts
@@ -1,7 +1,17 @@
 import styled from 'styled-components'
 
-export default styled.nav`
+export const Nav = styled.nav`
   width: 30%;
   max-width: 280px;
   padding-right: 35px;
+`
+
+export const NavSection = styled.div`
+  margin: 2.5rem 0 0 0;
+
+  ul {
+    margin: -1rem 0 0 0;
+    list-style: none;
+    padding: 0;
+  }
 `
