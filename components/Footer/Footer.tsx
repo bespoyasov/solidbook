69ad7f3432e0c62e32ecce93b174e696af383f0e
Александр <forjob159@gmail.com>
2019-03-06 17:35:05 +0200

    add subscriptions in footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,14 +1,23 @@
 import * as React from 'react'
 import { PureComponent } from 'react'
 import Center from '../layouts/Center'
-import FooterWrapper from './style'
+import { Wrapper, Container } from './style'
 
 class Footer extends PureComponent {
   render() {
     return (
-      <FooterWrapper>
-        <Center>А тут будет футер</Center>
-      </FooterWrapper>
+      <Wrapper>
+        <Center>
+          <Container>
+            <span>
+              <a href="https://bespoyasov.ru">Саша Беспоясов</a> и{' '}
+              <a href="https://github.com/dex157">Артём Самофалов</a>
+            </span>
+
+            <span>2019</span>
+          </Container>
+        </Center>
+      </Wrapper>
     )
   }
 }
--- components/Footer/style.ts
+++ components/Footer/style.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 
-export default styled.footer`
+export const Wrapper = styled.footer`
   border-top: 1px solid ${props => props.theme.lightestGrey};
   padding: 1rem 0 1.1rem;
+`
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
 `
