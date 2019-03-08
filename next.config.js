69828621cb779b57981f7aa2405d988060ad4d52
Александр <forjob159@gmail.com>
2019-03-08 20:35:45 +0300

    update next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const withTypescript = require('@zeit/next-typescript')
 const withSourceMaps = require('@zeit/next-source-maps')
+const withCSS = require('@zeit/next-css')
 require('dotenv').load()
 
-module.exports = withSourceMaps(withTypescript())
+module.exports = withCSS(withSourceMaps(withTypescript()))
