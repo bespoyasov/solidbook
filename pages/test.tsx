948d8614e65aaf4dfd3592d6cb494694bdf7b992
Александр <forjob159@gmail.com>
2019-03-11 20:54:26 +0200

    extract test form into a component

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,5 +1,6 @@
 import MainLayout from 'components/layouts/MainLayout'
 import Abbr from 'components/Formatters/Abbr'
+import TestForm from 'components/TestForm'
 import Code from 'components/Code'
 
 export default () => (
@@ -121,33 +122,7 @@ class TxtFormatter implements IFormatter {
 
     <section>
       <h2>Вопросы</h2>
-      <fieldset>
-        <legend>В чём главная причина того, что выбор формата данных вынесен в отдельный класс?</legend>
-        <label>
-          <input name="question-1" type="checkbox" />
-          <span>
-            он не относится ни к форматированию данных, ни к подготовке их к экспорту. По <Abbr>SRP</Abbr> его следует
-            держать отдельно от других сущностей
-          </span>
-        </label>
-        <label>
-          <input name="question-1" type="checkbox" />
-          <span>
-            потому что иначе код других классов сильно разрастается. Так без этого чтение и понимание становятся гораздо
-            труднее (да, но это лишь следствие основной причины — что выбор формата не относится ни к форматированию
-            данных, ни к подготовке их к экспорту)
-          </span>
-        </label>
-
-        <label>
-          <input name="question-1" type="checkbox" />
-          <span>
-            это удобнее с точки зрения управления зависимостями. В этом случае подключение функциональности выбора
-            формата производится в одном месте (да, но это следствие основной причины — выбор формата не относится ни к
-            форматированию данных, ни к подготовке их к экспорту, поэтому правильно его вынести в отдельный модуль)
-          </span>
-        </label>
-      </fieldset>
+      <TestForm />
     </section>
   </MainLayout>
 )
