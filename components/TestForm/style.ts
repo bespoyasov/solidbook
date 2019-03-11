8931175039ac21a06fc0bc496e2df0410239de84
Александр <forjob159@gmail.com>
2019-03-11 21:18:44 +0200

    add test form styles

---
+++ components/TestForm/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Container = styled.fieldset`
+  padding: 0;
+  margin: 0;
+  border: 0;
+`
+
+export const Heading = styled.legend`
+  padding: 0;
+  margin: 0;
+  font-weight: 700;
+`
