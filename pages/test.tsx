026481edfdc49571c6ed46e2b52dfdf3697f4af2
Александр <forjob159@gmail.com>
2019-03-11 20:50:46 +0200

    add more content and initial test markup

--- pages/test.tsx
+++ pages/test.tsx
@@ -16,13 +16,11 @@ export default () => (
 
     <section>
       <h2>Отчёт</h2>
-
       <p>
         Мы создадим класс <code>ReportExporter</code>, который будет заниматься только экспортом данных. Определять
         необходимый формат будет класс <code>FormatSelector</code>. А форматированием данных будут заниматься классы:{' '}
         <code>HtmlFormatter</code> и <code>TxtFormatter</code>.
       </p>
-
       <Code>{`// тип данных для отчёта
 type ReportData = {
   content: string,
@@ -42,16 +40,114 @@ class ReportExporter {
   data: ReportData
 
   constructor(name: string, data: ReportData) {
-      this.name = name
-      this.data = data
+    this.name = name
+    this.data = data
   }
 
   export(reportType: ReportTypes): string {
-      const formatter: IFormatter = new FormatSelector(reportType, this.data)
-      return formatter.format()
+    const formatter: IFormatter = new FormatSelector(reportType, this.data)
+    return formatter.format()
   }
 }
 `}</Code>
+    </section>
+
+    <section>
+      <h2>Форматы экспорта</h2>
+      <p>
+        В соответствии с <Abbr title="Single responsibility principle">SRP</Abbr> форматирование данных — это отдельная
+        задача. Поэтому для преобразования данных отчёта в необходимый формат мы создадим отдельные классы.
+      </p>
+      <Code>{`interface IFormatter {
+  data: ReportData;
+  format(): string;
+}
+
+// класс для форматирования в HTML
+class HtmlFormatter implements IFormatter {
+  data: ReportData
+
+  constructor(data: ReportData) {
+    this.data = data
+  }
+
+  format(): string {
+    // форматируем данные в HTML и возвращаем:
+    return 'html string'
+  }
+}
+
+// класс для форматирования в TXT
+class TxtFormatter implements IFormatter {
+  data: ReportData
+
+  constructor(data: ReportData) {
+    this.data = data
+  }
+
+  format(): string {
+    // форматируем данные в TXT и возвращаем:
+    return 'txt string'
+  }
+}`}</Code>
+    </section>
+
+    <section>
+      <h2>Выбор формата</h2>
+      <p>
+        Принцип единой ответственности подсказывает, что выбор формата не входит ни в задачу форматирования данных, ни в
+        задачу их подготовки. Поэтому существующие классы нам не подойдут.
+      </p>
+      <p>
+        Создадим новый класс <code>FormatSelector</code>, который будет выбирать тип форматирования, в зависимости от
+        настроек.
+      </p>
+      <Code>{`class FormatSelector {
+  formatters = {
+    ReportTypes.Html: HtmlFormatter,
+    ReportTypes.Txt: TxtFormatter,
+  }
+
+  constructor(reportType: ReportTypes, data: ReportData) {
+    const FormatterFactory = this.formatters[reportType]
+    return new FormatterFactory(data)
+  }
+}`}</Code>
+      <p>
+        Таким образом <Abbr>SRP</Abbr> помогает разделить ответственность за различные задачи между сущностями и сделать
+        это так, чтобы каждая сущность занималась одной задачей.
+      </p>
+    </section>
+
+    <section>
+      <h2>Вопросы</h2>
+      <fieldset>
+        <legend>В чём главная причина того, что выбор формата данных вынесен в отдельный класс?</legend>
+        <label>
+          <input name="question-1" type="checkbox" />
+          <span>
+            он не относится ни к форматированию данных, ни к подготовке их к экспорту. По <Abbr>SRP</Abbr> его следует
+            держать отдельно от других сущностей
+          </span>
+        </label>
+        <label>
+          <input name="question-1" type="checkbox" />
+          <span>
+            потому что иначе код других классов сильно разрастается. Так без этого чтение и понимание становятся гораздо
+            труднее (да, но это лишь следствие основной причины — что выбор формата не относится ни к форматированию
+            данных, ни к подготовке их к экспорту)
+          </span>
+        </label>
+
+        <label>
+          <input name="question-1" type="checkbox" />
+          <span>
+            это удобнее с точки зрения управления зависимостями. В этом случае подключение функциональности выбора
+            формата производится в одном месте (да, но это следствие основной причины — выбор формата не относится ни к
+            форматированию данных, ни к подготовке их к экспорту, поэтому правильно его вынести в отдельный модуль)
+          </span>
+        </label>
+      </fieldset>
     </section>
   </MainLayout>
 )
