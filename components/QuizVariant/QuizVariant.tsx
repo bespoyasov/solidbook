bc0e426f463f7d21aa2ceca2d64a6898cc6e8455
Александр <forjob159@gmail.com>
2019-03-12 14:07:44 +0200

    update QuizVariant

--- components/QuizVariant/QuizVariant.tsx
+++ components/QuizVariant/QuizVariant.tsx
@@ -1,22 +1,27 @@
 import * as React from 'react'
 import { PureComponent } from 'react'
+import VisuallyHidden from 'components/VisuallyHidden'
 import { Item } from './style'
 
 type Props = {
   children?: React.ReactNode
   name: string
-  value: string
+  value: string | number
+  selected: boolean
 }
 
 class TestItem extends PureComponent<Props> {
   static defaultProps = {}
 
   render() {
-    const { children, name, value } = this.props
+    const { children, name, value, selected } = this.props
 
     return (
-      <Item>
-        <input type="checkbox" name={name} value={value} />
+      <Item className={selected ? 'selected' : ''}>
+        <VisuallyHidden>
+          <input type="checkbox" name={name} value={value} defaultChecked={selected} />
+        </VisuallyHidden>
+
         <div className="indicator" />
         <span>{children}</span>
       </Item>
--- components/QuizVariant/style.ts
+++ components/QuizVariant/style.ts
@@ -4,7 +4,19 @@ export const Item = styled.label`
   display: block;
   user-select: none;
   cursor: pointer;
-  padding: 0.3em 0.6em;
-  margin-bottom: 0.5em;
+  padding: 0.5rem 0.8rem;
+  margin: 0 0 1rem 0;
   position: relative;
+  border: 1px solid ${props => props.theme.lightGrey};
+  border-radius: ${props => props.theme.radius};
+  line-height: 1.25;
+
+  &:hover,
+  &:focus-within {
+    background: ${props => props.theme.lightBlueOpaque};
+  }
+
+  &.selected {
+    border-color: black;
+  }
 `
