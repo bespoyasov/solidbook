6e7921f52a0e8f172d895c1a509dcaa0d0c51b8f
Александр <forjob159@gmail.com>
2019-03-12 13:50:28 +0200

    rename TestItem into QuizVariant

--- components/Quiz/Quiz.tsx
+++ components/Quiz/Quiz.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { PureComponent } from 'react'
 import Abbr from 'components/Formatters/Abbr'
-import TestItem from 'components/TestItem'
+import QuizVariant from 'components/QuizVariant'
 import { Container, Heading } from './style'
 
 class TestForm extends PureComponent {
@@ -9,19 +9,19 @@ class TestForm extends PureComponent {
     return (
       <Container>
         <Heading>В чём главная причина того, что выбор формата данных вынесен в отдельный класс?</Heading>
-        <TestItem name="srp-1" value="a">
+        <QuizVariant name="srp-1" value="a">
           Он не относится ни к форматированию данных, ни к подготовке их к экспорту. По <Abbr>SRP</Abbr> его следует
           держать отдельно от других сущностей
-        </TestItem>
-        <TestItem name="srp-1" value="b">
+        </QuizVariant>
+        <QuizVariant name="srp-1" value="b">
           Потому что иначе код других классов сильно разрастается. Так без этого чтение и понимание становятся гораздо
           труднее
-        </TestItem>
+        </QuizVariant>
         {/* (да, но это лишь следствие основной причины — что выбор формата не относится ни к форматированию данных, ни к подготовке их к экспорту) */}
-        <TestItem name="srp-1" value="c">
+        <QuizVariant name="srp-1" value="c">
           Это удобнее с точки зрения управления зависимостями. В этом случае подключение функциональности выбора формата
           производится в одном месте
-        </TestItem>
+        </QuizVariant>
         {/* (да, но это следствие основной причины — выбор формата не относится ни к
           форматированию данных, ни к подготовке их к экспорту, поэтому правильно его вынести в отдельный модуль) */}
       </Container>
