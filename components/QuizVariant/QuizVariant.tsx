8106aad2e7d6b0b3ee4aa7a24e6fbaf36725a16c
Александр <forjob159@gmail.com>
2019-03-15 14:14:28 +0200

    convert component to fsc, add context vis useContext hook

--- components/QuizVariant/QuizVariant.tsx
+++ components/QuizVariant/QuizVariant.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react'
-import { PureComponent } from 'react'
+import { useContext } from 'react'
 import clsx from 'clsx'
 import VisuallyHidden from '~/components/VisuallyHidden'
 import { Label, Description, Item } from './style'
 import { QuizVariantData } from './types'
+import QuizContext from '../../context/quiz'
 
 type Props = {
   question: string
@@ -11,28 +12,25 @@ type Props = {
   variant: QuizVariantData
 }
 
-class QuizVariant extends PureComponent<Props> {
-  static defaultProps = {
-    completed: false
-  }
+function QuizVariant(props: Props) {
+  const { question, completed, variant } = props
+  const { main, description, id } = variant
 
-  render() {
-    const { question, completed, variant } = this.props
-    const { selected, correct, main, description } = variant
+  const context = useContext(QuizContext)
+  const { selected, correct } = context[id]
 
-    return (
-      <Item>
-        <Label className={clsx({ selected }, { correct }, { completed })}>
-          <VisuallyHidden>
-            <input type="checkbox" name={question} value={main} defaultChecked={selected} disabled={completed} />
-          </VisuallyHidden>
+  return (
+    <Item>
+      <Label className={clsx({ selected }, { correct }, { completed })}>
+        <VisuallyHidden>
+          <input type="checkbox" name={question} value={main} defaultChecked={selected} disabled={completed} />
+        </VisuallyHidden>
 
-          <span>{main}</span>
-        </Label>
-        {!!description && completed && <Description>{description}</Description>}
-      </Item>
-    )
-  }
+        <span>{main}</span>
+      </Label>
+      {!!description && completed && <Description>{description}</Description>}
+    </Item>
+  )
 }
 
 export default QuizVariant
