407fa7dc50ad83588e251b0327459e73dfbb251a
Александр <forjob159@gmail.com>
2019-03-16 12:00:39 +0200

    extract routes into other file

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -3,34 +3,7 @@ import { PureComponent } from 'react'
 import PointsCounter from '../PointsCounter'
 import { Nav, Section, SubSection } from './style'
 import NavigationItem from '../NavigationItem'
-
-type LinkShape = {
-  link: string
-  name: string
-  subnav?: LinkShape[]
-}
-
-const link = (link: string, name: string, subnav?: LinkShape[]): LinkShape => ({
-  link,
-  name,
-  subnav
-})
-
-const routes = [
-  link('/', 'Введение'),
-  link('/srp', 'Принцип единственной ответственности', [
-    link('/srp/intro', 'Введение и понятия'),
-    link('/srp/in-ideal-world', 'Примеры из идеального мира'),
-    link('/srp/in-real-life', 'Примеры из реальной жизни'),
-    link('/srp/patterns', 'Шаблоны проектирования и приёмы рефакторинга'),
-    link('/srp/antipatterns', 'Антипаттерны'),
-    link('/srp/limits-and-caveats', 'Ограничения и подводные камни')
-  ]),
-  link('/open-closed', 'Принцип открытости и закрытости'),
-  link('/liskov-substitution', 'Принцип подстановки Барбары Лисков'),
-  link('/isp', 'Принцип разделения интерфейса'),
-  link('/di', 'Принцип инверсии зависимостей')
-]
+import routes from './routes'
 
 class Navigation extends PureComponent {
   render() {
---
+++ components/Navigation/routes.ts
@@ -0,0 +1,56 @@
+type RouteShape = {
+  link: string
+  name: string
+  subnav?: RouteShape[]
+}
+
+const route = (link: string, name: string, subnav?: RouteShape[]): RouteShape => ({
+  link,
+  name,
+  subnav
+})
+
+export default [
+  route('/', 'Введение'),
+  route('/srp', 'Принцип единственной ответственности', [
+    route('/srp/intro', 'Введение и понятия'),
+    route('/srp/in-ideal-world', 'Примеры из идеального мира'),
+    route('/srp/in-real-life', 'Примеры из реальной жизни'),
+    route('/srp/patterns', 'Шаблоны проектирования и приёмы рефакторинга'),
+    route('/srp/antipatterns', 'Антипаттерны'),
+    route('/srp/limits-and-caveats', 'Ограничения и подводные камни')
+  ]),
+  route('/open-closed', 'Принцип открытости и закрытости', [
+    route('/open-closed/intro', 'Введение и понятия'),
+    route('/open-closed/in-ideal-world', 'Примеры из идеального мира'),
+    route('/open-closed/in-real-life', 'Примеры из реальной жизни'),
+    route('/open-closed/patterns', 'Шаблоны проектирования и приёмы рефакторинга'),
+    route('/open-closed/antipatterns', 'Антипаттерны'),
+    route('/open-closed/limits-and-caveats', 'Ограничения и подводные камни')
+  ]),
+  route('/liskov-substitution', 'Принцип подстановки Барбары Лисков', [
+    route('/liskov-substitution/intro', 'Введение и понятия'),
+    route('/liskov-substitution/in-ideal-world', 'Примеры из идеального мира'),
+    route('/liskov-substitution/in-real-life', 'Примеры из реальной жизни'),
+    route('/liskov-substitution/patterns', 'Шаблоны проектирования и приёмы рефакторинга'),
+    route('/liskov-substitution/antipatterns', 'Антипаттерны'),
+    route('/liskov-substitution/limits-and-caveats', 'Ограничения и подводные камни')
+  ]),
+  route('/isp', 'Принцип разделения интерфейса', [
+    route('/isp/intro', 'Введение и понятия'),
+    route('/isp/in-ideal-world', 'Примеры из идеального мира'),
+    route('/isp/in-real-life', 'Примеры из реальной жизни'),
+    route('/isp/patterns', 'Шаблоны проектирования и приёмы рефакторинга'),
+    route('/isp/antipatterns', 'Антипаттерны'),
+    route('/isp/limits-and-caveats', 'Ограничения и подводные камни')
+  ]),
+  route('/di', 'Принцип инверсии зависимостей', [
+    route('/di/intro', 'Введение и понятия'),
+    route('/di/in-ideal-world', 'Примеры из идеального мира'),
+    route('/di/in-real-life', 'Примеры из реальной жизни'),
+    route('/di/patterns', 'Шаблоны проектирования и приёмы рефакторинга'),
+    route('/di/antipatterns', 'Антипаттерны'),
+    route('/di/limits-and-caveats', 'Ограничения и подводные камни')
+  ]),
+  route('/afterwords', 'Заключение')
+]
