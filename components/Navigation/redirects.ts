74ac1b690e060b9a00b33259def8225bd791bd71
Александр <forjob159@gmail.com>
2019-03-16 12:26:12 +0200

    add redirects list

---
+++ components/Navigation/redirects.ts
@@ -0,0 +1,8 @@
+export default [
+  { from: '/', to: '/preface' },
+  { from: '/srp', to: '/srp/intro' },
+  { from: '/open-closed', to: '/open-closed/intro' },
+  { from: '/liskov-substitution', to: '/liskov-substitution/intro' },
+  { from: '/isp', to: '/isp/intro' },
+  { from: '/di', to: '/di/intro' }
+]
