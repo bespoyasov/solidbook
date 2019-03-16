554ec56e0d8410ac2e8220ebd6de87ffd50815bb
Александр <forjob159@gmail.com>
2019-03-16 13:03:14 +0200

    etract use redirect into a file

--- components/Navigation/redirects.ts
+++
@@ -1,7 +0,0 @@
-export default [
-  { from: '/srp', to: '/srp/intro' },
-  { from: '/open-closed', to: '/open-closed/intro' },
-  { from: '/liskov-substitution', to: '/liskov-substitution/intro' },
-  { from: '/isp', to: '/isp/intro' },
-  { from: '/di', to: '/di/intro' }
-]
--- pages/srp/index.tsx
+++ pages/srp/index.tsx
@@ -1,29 +1,5 @@
-import { useEffect } from 'react'
-import Router, { withRouter } from 'next/router'
-
-const redirects = [
-  { from: '/srp', to: '/srp/intro' },
-  { from: '/open-closed', to: '/open-closed/intro' },
-  { from: '/liskov-substitution', to: '/liskov-substitution/intro' },
-  { from: '/isp', to: '/isp/intro' },
-  { from: '/di', to: '/di/intro' }
-]
-
-function useRedirect(res, router) {
-  const { pathname } = router
-  const redirect = redirects.filter(({ from }) => from === pathname)[0]
-
-  useEffect(() => {
-    if (res) {
-      res.writeHead(302, {
-        Location: redirect.to
-      })
-      res.end()
-    } else {
-      Router.push(redirect.to)
-    }
-  })
-}
+import { withRouter } from 'next/router'
+import useRedirect from '../../hooks/useRedirect'
 
 function Redirect({ res, router }) {
   useRedirect(res, router)
