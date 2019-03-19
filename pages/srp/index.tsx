86a17e9cda515dae99772f55cae0c146835c2561
Александр <forjob159@gmail.com>
2019-03-19 19:36:43 +0200

    remove hooks and index redirect

--- pages/srp/index.tsx
+++
@@ -1,9 +0,0 @@
-import { withRouter } from 'next/router'
-import useRedirect from '../../hooks/useRedirect'
-
-function Redirect({ res, router }) {
-  useRedirect(res, router)
-  return null
-}
-
-export default withRouter(Redirect)
