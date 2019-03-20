314b560fc8d14653410278b933672e314e720aeb
Александр <forjob159@gmail.com>
2019-03-20 20:48:52 +0200

    add lib folder in tests config

--- jest.config.js
+++ jest.config.js
@@ -110,7 +110,7 @@ module.exports = {
   // rootDir: null,
 
   // A list of paths to directories that Jest should use to search for files in
-  roots: ['<rootDir>/domain', '<rootDir>/components', '<rootDir>/pages'],
+  roots: ['<rootDir>/domain', '<rootDir>/components', '<rootDir>/pages', '<rootDir>/lib'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
