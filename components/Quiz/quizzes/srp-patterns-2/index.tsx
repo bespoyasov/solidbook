12a60b560ca4a2b351c4ff8c3a5de958b6baef00
Александр <forjob159@gmail.com>
2019-05-03 14:52:05 +0200

    add srp patterns quizzes

---
+++ components/Quiz/quizzes/srp-patterns-2/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { IQuiz } from '../IQuiz'
+import Question from './question.mdx'
+import Variant1 from './variants/variant-1.mdx'
+import Variant2 from './variants/variant-2.mdx'
+import Variant3 from './variants/variant-3.mdx'
+
+const quiz: IQuiz = {
+  name: 'srp-patterns-2',
+  question: <Question />,
+  variants: [
+    {
+      text: <Variant1 />,
+      description: 'Он относится к проигрыванию трека, а это основное назначение плеера'
+    },
+    {
+      text: <Variant2 />,
+      description: 'Он относится к остановке проигрывания трека, это тоже основное назначение плеера'
+    },
+    {
+      text: <Variant3 />,
+      description: 'Да, лучше сделать отдельный класс, который будет работать с загрузками'
+    }
+  ],
+  meta: {
+    correctAnswers: [2]
+  }
+}
+
+export default quiz
---
+++ components/Quiz/quizzes/srp-patterns-8/index.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+import { IQuiz } from '../IQuiz'
+import Variant1 from './variants/variant-1.mdx'
+import Variant2 from './variants/variant-2.mdx'
+import Variant3 from './variants/variant-3.mdx'
+
+const quiz: IQuiz = {
+  name: 'srp-patterns-8',
+  question:
+    'Допустим, нам необходимо создать прокси, который бы кешировал ответы на запросы. Какой из вариантов реализации не содержит ошибок?',
+  variants: [
+    {
+      text: <Variant1 />
+    },
+    {
+      text: <Variant2 />,
+      description: 'Этот вариант всегда ссылается на значение из кэша, даже если его нет'
+    },
+    {
+      text: <Variant3 />,
+      description:
+        'Этот вариант вначале делает запрос, а потом проверяет, есть ли значение в кэше — то есть суть кэша исчезает'
+    }
+  ],
+  meta: {
+    correctAnswers: [0]
+  }
+}
+
+export default quiz
