cb1ad729de163c45256c00e1f2b79f9d662bb718
Александр <forjob159@gmail.com>
2019-05-03 13:52:36 +0200

    update quiz button styles

--- components/Quiz/components/QuizContainer/style.ts
+++ components/Quiz/components/QuizContainer/style.ts
@@ -1,13 +1,17 @@
 import styled from 'styled-components'
 
 export const Container = styled.fieldset`
-  margin: 0 0 1rem 0;
+  margin: 0 0 1.5rem 0;
   padding: 0;
   border: 0;
+
+  button {
+    margin-top: -0.5rem;
+  }
 `
 
 export const Heading = styled.legend`
   padding: 0;
-  margin: 0 0 1rem;
+  margin: 0 0 0.5rem;
   font-weight: 700;
 `
