c47cbd83457078029a2a66a99e12e5e06b61ba74
Александр <forjob159@gmail.com>
2019-07-03 23:22:45 +0300

    extract common theme props into a file

---
+++ themes/common/index.ts
@@ -0,0 +1,12 @@
+export const common = {
+  radius: '0.3em',
+
+  font: `-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif`,
+  fontMonospace: `Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace`,
+
+  fontSizeRegular: `1rem`,
+  fontSizeSmall: `0.9rem`,
+  fontSizeTiny: `0.8rem`,
+
+  breakpoint: '960px'
+}
