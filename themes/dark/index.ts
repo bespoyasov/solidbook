08b14facf2552de2357398301a17fc713079c1f1
Александр <forjob159@gmail.com>
2019-07-03 23:22:53 +0300

    create light and dark themes

---
+++ themes/dark/index.ts
@@ -0,0 +1,28 @@
+import { palette } from '../palette'
+
+export const theme = {
+  textColorPrimary: 'white',
+  textColorSecondary: palette.hoar,
+
+  linkColor: palette.blueAlt,
+  linkVisitedColor: palette.purpleAlt,
+  linkHoverColor: palette.redAlt,
+  buttonColor: 'black',
+  disabledButtonColor: palette.grey,
+
+  pageBackground: palette.violet,
+  codeBackground: palette.deep,
+  buttonBackground: 'white',
+  imageBackground: 'white',
+
+  decorationColorPrimary: palette.lightHoar,
+  decorationColorSecondary: palette.lightHoar,
+  decorationColorTetriary: palette.hoar,
+
+  variantHoverBackground: palette.lightHoar,
+  variantSelectedBorderColor: palette.silver,
+  navBorderColor: palette.lightHoarOpqaue,
+  navActiveItemBackground: 'white',
+  navCompletedDecorationColor: 'white',
+  navCompletedDecorationBackground: palette.violet
+}
---
+++ themes/light/index.ts
@@ -0,0 +1,27 @@
+import { palette } from '../palette'
+
+export const theme = {
+  textColorPrimary: 'black',
+  textColorSecondary: palette.grey,
+
+  linkColor: palette.blue,
+  linkVisitedColor: palette.purple,
+  linkHoverColor: palette.red,
+  buttonColor: 'white',
+  disabledButtonColor: 'rgba(255, 255, 255, 0.8)',
+
+  pageBackground: 'white',
+  codeBackground: palette.lightBlueOpaque,
+  buttonBackground: 'black',
+
+  decorationColorPrimary: palette.lightGrey,
+  decorationColorSecondary: palette.lightestGrey,
+  decorationColorTetriary: palette.grey,
+
+  variantHoverBackground: palette.lightBlueOpaque,
+  variantSelectedBorderColor: 'black',
+  navBorderColor: palette.lightGreyOpaque,
+  navActiveItemBackground: 'black',
+  navCompletedDecorationBackground: palette.green,
+  navCompletedDecorationColor: 'white'
+}
