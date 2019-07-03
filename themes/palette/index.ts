4572faf1f31d86bb9df2cd6a39bf7f2af3fb792a
Александр <forjob159@gmail.com>
2019-07-03 23:22:28 +0300

    extract colors into palette and add some new colors

---
+++ themes/palette/index.ts
@@ -0,0 +1,24 @@
+export const palette = {
+  blue: '#0000fe',
+  purple: '#542189',
+  red: '#fb3347',
+  green: '#36bc54',
+  violet: '#282a35',
+  deep: '#353744',
+
+  blueAlt: '#00a1fe',
+  purpleAlt: '#bd93f9',
+  redAlt: '#ff5555',
+
+  lightestGrey: 'rgba(0, 0, 0, 0.06)',
+  lightGrey: 'rgba(0, 0, 0, 0.12)',
+  grey: 'rgba(0, 0, 0, 0.3)',
+
+  lightHoar: 'rgba(255,255,255,0.05)',
+  hoar: 'rgba(255, 255, 255, 0.4)',
+  silver: 'rgba(255, 255, 255, 0.6)',
+
+  lightGreyOpaque: '#f0f0f0',
+  lightBlueOpaque: '#f5f7ff',
+  lightHoarOpqaue: '#32343e'
+}
