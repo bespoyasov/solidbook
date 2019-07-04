7cea23d6af9d4d033d1099a3834b7bb3539725be
Александр <forjob159@gmail.com>
2019-07-04 23:49:27 +0300

    update toggle container styles

--- components/ThemeToggle/style.ts
+++ components/ThemeToggle/style.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: inline-flex;
+`
 
 export const Toggle = styled.label`
   display: flex;
