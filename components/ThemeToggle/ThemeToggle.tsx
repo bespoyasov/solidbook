f807c59afb385f17e9b9e52e1e4c1588203c0975
Александр <forjob159@gmail.com>
2019-07-05 22:38:52 +0300

    connect theme toggler to model

--- components/ThemeToggle/ThemeToggle.tsx
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react'
+import clsx from 'clsx'
+import { inject, observer } from 'mobx-react'
+import { Instance } from 'mobx-state-tree'
 import VisuallyHidden from '~/components/VisuallyHidden'
+import { ThemeModel } from '~/models/theme'
 import { Container, Toggle, Shaft, Trolley } from './style'
 
-export default class ThemeToggle extends React.PureComponent {
+interface InjectedProps {
+  theme: Instance<typeof ThemeModel>
+}
+
+class ThemeToggle extends React.Component {
+  get injected() {
+    return this.props as InjectedProps
+  }
+
   render() {
+    const { using } = this.injected.theme
+    const usingDark = using === 'dark'
+
     return (
       <Container>
         <VisuallyHidden>
-          <input id="toggle" type="checkbox" />
+          <input id="toggle" type="checkbox" onChange={this.injected.theme.toggleTheme} checked={usingDark} />
         </VisuallyHidden>
         <Toggle aria-label="Переключить тему" role="button" htmlFor="toggle">
           <Shaft>
-            <Trolley />
+            <Trolley className={clsx({ active: usingDark })} />
           </Shaft>
         </Toggle>
       </Container>
     )
   }
 }
+
+export default inject('theme')(observer(ThemeToggle))
