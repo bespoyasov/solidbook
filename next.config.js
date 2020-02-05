52eb96cb694c78c743e181fe5fbed4862283a9a1
Artem Samofalov <dex157@gmail.com>
2020-02-06 01:11:35 +0500

    replace markdown plugins

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 const withSourceMaps = require('@zeit/next-source-maps')
 const withCSS = require('@zeit/next-css')
 
-const markdownPluginAbbr = require('remark-abbr')
-const markdownPluginSectionize = require('remark-sectionize')
-
 const withMDX = require('@next/mdx')({
   extension: /\.(md|mdx)$/,
   options: {
-    mdPlugins: [markdownPluginAbbr, markdownPluginSectionize]
+    remarkPlugins: [require('remark-abbr'), require('remark-sectionize')]
   }
 })
 
