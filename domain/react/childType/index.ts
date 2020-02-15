bd5f7aeafbe4c5b263c2d0f17d07a91bab7b46a9
Artem Samofalov <dex157@gmail.com>
2020-02-15 18:06:14 +0300

    revert domain/react/childType/index.ts

--- domain/react/childType/index.ts
+++ domain/react/childType/index.ts
@@ -16,21 +16,21 @@ export enum TYPES {
 export function getType(node: ReactNode) {
   if (isNull(node)) {
     return TYPES.NULL
-  } if (typeof node === 'boolean') {
+  } else if (typeof node === 'boolean') {
     return TYPES.BOOLEAN
-  } if (typeof node === 'string') {
+  } else if (typeof node === 'string') {
     return TYPES.STRING
-  } if (Array.isArray(node)) {
+  } else if (Array.isArray(node)) {
     return TYPES.ARRAY
-  } if (typeof node === 'number') {
+  } else if (typeof node === 'number') {
     return TYPES.NUMBER
-  } if (isValidElement(node)) {
+  } else if (isValidElement(node)) {
     if (node.props.children != null) {
       return TYPES.REACT_ELEMENT_WITH_CHILDREN
-    } 
+    } else {
       return TYPES.REACT_ELEMENT_WITHOUT_CHILDREN
-    
-  } 
+    }
+  } else {
     return TYPES.UNDEFINED
-  
+  }
 }
