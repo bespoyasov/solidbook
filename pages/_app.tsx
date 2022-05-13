a7b427bd8dc6c3763ff758c4a98f4dc7f5045646
Alexander Bespoyasov <forjob159@gmail.com>
2022-05-13 10:47:21 +0200

    Fix compile error related to the `meta` tag in `head`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Provider as MobxProvider } from 'mobx-react'
 import { Instance } from 'mobx-state-tree'
+import Head from 'next/head'
 import App from 'next/app'
 import { AppContextType } from 'next/dist/next-server/lib/utils'
 import { Router } from 'next/router'
@@ -59,6 +60,30 @@ export default class MyApp extends App<MyAppInitialProps> {
 
     return (
       <>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#f8364c" />
+          <meta name="msapplication-TileColor" content="#f8364c" />
+          <meta name="theme-color" content="#ffffff" />
+
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content="SOLID BOOK" />
+          <meta property="og:site_name" content="Open Tech Authors, SOLID" />
+          <meta property="og:url" content="https://ota-solid.now.sh" />
+          <meta property="og:image" content="https://ota-solid.now.sh/socials.png" />
+          <meta property="article:author" content="https://bespoyasov.ru" />
+          <meta property="article:author" content="https://github.com/dex157" />
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="https://ota-solid.now.sh" />
+          <meta name="twitter:title" content="SOLID BOOK" />
+          <meta name="twitter:description" content="Книга о принципах объектно-ориентированного дизайна" />
+          <meta name="twitter:image" content="https://ota-solid.now.sh/socials.png" />
+        </Head>
         <MobxProvider {...this.models}>
           <>
             <ServicesManager />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -23,28 +23,6 @@ export default class MyDocument extends Document<IProps> {
     return (
       <html lang="ru">
         <Head>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#f8364c" />
-          <meta name="msapplication-TileColor" content="#f8364c" />
-          <meta name="theme-color" content="#ffffff" />
-
-          <meta property="og:type" content="article" />
-          <meta property="og:title" content="SOLID BOOK" />
-          <meta property="og:site_name" content="Open Tech Authors, SOLID" />
-          <meta property="og:url" content="https://ota-solid.now.sh" />
-          <meta property="og:image" content="https://ota-solid.now.sh/socials.png" />
-          <meta property="article:author" content="https://bespoyasov.ru" />
-          <meta property="article:author" content="https://github.com/dex157" />
-
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="https://ota-solid.now.sh" />
-          <meta name="twitter:title" content="SOLID BOOK" />
-          <meta name="twitter:description" content="Книга о принципах объектно-ориентированного дизайна" />
-          <meta name="twitter:image" content="https://ota-solid.now.sh/socials.png" />
           <style>
             {`
             *,
