807c7ab26cdbcbf5be1334f8a23d6f2071b490f1
Alex Bespoyasov <bespoyasov@me.com>
2023-07-01 14:16:15 +0200

    Adjust gaps between share buttons

--- components/Share/style.ts
+++ components/Share/style.ts
@@ -7,15 +7,10 @@ export const Container = styled.div`
   .likely__counter {
     display: none !important;
   }
-
-  .likely .likely__widget {
-    margin-left: 4px;
-    margin-right: 4px;
-  }
 `
 
 export const Label = styled.div`
-  @media (max-width: ${props => props.theme.breakpoint}) {
+  @media (max-width: ${(props) => props.theme.breakpoint}) {
     display: none;
   }
 `
